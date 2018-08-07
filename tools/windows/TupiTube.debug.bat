8793460e7900c84c468da39e9b8239a02312fa0f
Gustav Gonzalez <xtingray@gmail.com>
2018-08-07 14:28:28 -0500

    Working on Windows debugging tools

---
+++ tools/windows/TupiTube.debug.bat
@@ -0,0 +1,5 @@
+set TUPITUBE_HOME=..
+set TUPITUBE_SHARE=%TUPITUBE_HOME%\data
+set TUPITUBE_PLUGIN=%TUPITUBE_HOME%\plugins
+set PATH=%TUPITUBE_HOME%\bin\raw;%TUPITUBE_HOME%\lib;%TUPITUBE_HOME%\lib\ssl;%TUPITUBE_HOME%\lib\qt5;%TUPITUBE_HOME%\lib\libav
+tupitube.desk.exe 2> log.txt
