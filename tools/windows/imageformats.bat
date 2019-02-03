2362cc2ed7f35d3727312a4465e0fee352b71a1c
Gustav Gonzalez <xtingray@gmail.com>
2019-02-03 10:28:09 -0500

    Adding new scripts for the Windows installer

---
+++ tools/windows/imageformats.bat
@@ -0,0 +1,5 @@
+REM Developed by Santiago Paz Mendieta
+
+copy C:\devel\Qt\5.12.0\mingw73_64\plugins\imageformats\qgif.dll C:\devel\sources\imageformats
+copy C:\devel\Qt\5.12.0\mingw73_64\plugins\imageformats\qjpeg.dll C:\devel\sources\imageformats
+copy C:\devel\Qt\5.12.0\mingw73_64\plugins\imageformats\qsvg.dll C:\devel\sources\imageformats
