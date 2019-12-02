7adf41bfa0c3630a1e39e982b2189dc7a4813100
Gustav Gonzalez <xtingray@gmail.com>
2019-12-01 20:14:18 -0500

    Updating Windows script files

--- tools/windows/TupiTube.bat
+++ tools/windows/TupiTube.bat
@@ -1,3 +1,3 @@
 @echo OFF
-set PATH=%TUPITUBE_HOME%\bin\raw;%TUPITUBE_HOME%\lib;%TUPITUBE_HOME%\lib\ssl;%TUPITUBE_HOME%\lib\qt5;%TUPITUBE_HOME%\lib\ffmpeg
+set PATH=%TUPITUBE_HOME%\bin\raw;%TUPITUBE_HOME%\lib;%TUPITUBE_HOME%\lib\ssl;%TUPITUBE_HOME%\lib\qt5;%TUPITUBE_HOME%\lib\ffmpeg;%TUPITUBE_HOME%\lib\raster
 start tupitube.desk.exe %1
--- tools/windows/TupiTube.debug.bat
+++ tools/windows/TupiTube.debug.bat
@@ -1,5 +1,5 @@
 set TUPITUBE_HOME=..
 set TUPITUBE_SHARE=%TUPITUBE_HOME%\data
 set TUPITUBE_PLUGIN=%TUPITUBE_HOME%\plugins
-set PATH=%TUPITUBE_HOME%\bin\raw;%TUPITUBE_HOME%\lib;%TUPITUBE_HOME%\lib\ssl;%TUPITUBE_HOME%\lib\qt5;%TUPITUBE_HOME%\lib\ffmpeg
+set PATH=%TUPITUBE_HOME%\bin\raw;%TUPITUBE_HOME%\lib;%TUPITUBE_HOME%\lib\ssl;%TUPITUBE_HOME%\lib\qt5;%TUPITUBE_HOME%\lib\ffmpeg;%TUPITUBE_HOME%\lib\raster
 tupitube.desk.exe 2> log.txt
