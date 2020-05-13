4ceb19d38b039547910f366cd950f160670f232c
xtingray <xtingray@gmail.com>
2020-05-13 10:43:39 -0500

    Adding TLabel class

---
+++ src/framework/gui/tlabel.h
@@ -0,0 +1,58 @@
+/***************************************************************************
+ *   Project TUPITUBE DESK                                                *
+ *   Project Contact: info@maefloresta.com                                 *
+ *   Project Website: http://www.maefloresta.com                           *
+ *   Project Leader: Gustav Gonzalez <info@maefloresta.com>                *
+ *                                                                         *
+ *   Developers:                                                           *
+ *   2010:                                                                 *
+ *    Gustavo Gonzalez / xtingray                                          *
+ *                                                                         *
+ *   KTooN's versions:                                                     * 
+ *                                                                         *
+ *   2006:                                                                 *
+ *    David Cuadrado                                                       *
+ *    Jorge Cuadrado                                                       *
+ *   2003:                                                                 *
+ *    Fernado Roldan                                                       *
+ *    Simena Dinas                                                         *
+ *                                                                         *
+ *   Copyright (C) 2010 Gustav Gonzalez - http://www.maefloresta.com       *
+ *   License:                                                              *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#ifndef TLABEL_H
+#define TLABEL_H
+
+#include <QLabel>
+#include <QMouseEvent>
+
+class TLabel : public QLabel {
+
+    Q_OBJECT
+
+    public:
+        TLabel(QWidget *parent = nullptr, Qt::WindowFlags flags = Qt::Widget);
+        TLabel(const QString &text, QWidget *parent = nullptr, Qt::WindowFlags flags = Qt::Widget);
+        ~TLabel();
+
+    protected:
+        void mousePressEvent(QMouseEvent *);
+
+    signals:
+        void clicked();
+};
+
+#endif
