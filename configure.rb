fe8fc80146b57eb3494aaf723edfba1759ec1c17
xtingray <xtingray@gmail.com>
2020-08-02 23:49:27 -0500

    Preparing release 0.2.16 (Gambá)

--- configure.rb
+++ configure.rb
@@ -162,8 +162,8 @@ _EOH_
     end
 
     config.addDefine('TUPITUBE_VERSION=\\\\\"0.2\\\\\"')
-    config.addDefine('CODE_NAME=\\\\\"Pytuna\\\\\"')
-    config.addDefine('REVISION=\\\\\"15\\\\\"')
+    config.addDefine('CODE_NAME=\\\\\"Gambá\\\\\"')
+    config.addDefine('REVISION=\\\\\"16\\\\\"')
     config.addDefine('CONFIG_VERSION=\\\\\"3\\\\\"')
 
     if File.exists?('/etc/canaima_version')
