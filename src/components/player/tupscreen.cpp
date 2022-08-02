7974f1fba535050415fb20591e475c1cf59fdff5
Gustav Gonzalez <gg@dyhrbergsystems.com>
2022-08-02 09:34:08 -0500

    Control commit

--- src/components/player/tupscreen.cpp
+++ src/components/player/tupscreen.cpp
@@ -836,6 +836,7 @@ void TupScreen::playSoundAt(int frame)
                     if (soundPlayer.at(i)->state() != QMediaPlayer::PlayingState) {
                         #ifdef TUP_DEBUG
                             qWarning() << "[TupScreen::playSoundAt()] - Playing file -> " << soundRecord.path;
+                            qWarning() << "[TupScreen::playSoundAt()] - frame -> " << frame;
                         #endif
                         soundPlayer.at(i)->setMedia(QUrl::fromLocalFile(soundRecord.path));
                         soundPlayer.at(i)->play();
